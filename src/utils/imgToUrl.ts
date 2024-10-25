bcc4b57df589f76d920466eccc8e736b30df8e8b
yimco <yimco@gmail.com>
2024-10-25 01:57:15 -0500

    mi db funcional railway y vercel cambie db

--- src/utils/imgToUrl.ts
+++ src/utils/imgToUrl.ts
@@ -1,7 +1,7 @@
 import { HttpStatus, HttpException } from '@nestjs/common';
 
 export const imgToUrl = async (img: any) => {
-  const KEYBB: string = process.env.IMGBB_APIKEY;
+  const KEYBB: string = 'dde7c8307d66b5eff422baf53acdb36f';
   const url: string = 'https://api.imgbb.com/1/upload';
 
   // profile_img.mimetype,
