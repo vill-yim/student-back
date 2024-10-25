c15f1fccdac281120feb3e14a3ace073242eaa68
yimco <yimco@gmail.com>
2024-10-25 02:23:15 -0500

    mi db funcional railway y vercel x2

--- src/typeormservice.ts
+++ src/typeormservice.ts
@@ -47,7 +47,7 @@ export class TypeOrmService implements TypeOrmOptionsFactory {
      */
     return {
       type: 'postgres',
-      url: this.configService.get<any>('POSTGRES_URL'),
+      url: this.configService.get<any>('HACKA_DATABASE_URL'),
       entities: [Student, Signature, Task, Grade, Feedback],
       synchronize: true,
     };
